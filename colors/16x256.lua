d50510a500a9a9045071ed0ce114fa75c9ad9f48
Göran Gustafsson <gustafsson.g@gmail.com>
2022-04-22 17:48:17 +0200

    ...

--- colors/16x256.lua
+++ colors/16x256.lua
@@ -23,19 +23,23 @@ local black = 16  -- #000000
 local grey  = 242
 local white = 231 -- #ffffff
 
-local dark_red     = 1 -- Dark Red
-local dark_green   = 2 -- Dark Green
-local dark_yellow  = 3 -- Dark Yellow
-local dark_blue    = 4 -- Dark Blue
-local dark_magenta = 5 -- Dark Magenta
-local dark_cyan    = 6 -- Dark Cyan
+local dark_black   = 0
+local dark_red     = 1
+local dark_green   = 2
+local dark_yellow  = 3
+local dark_blue    = 4
+local dark_magenta = 5
+local dark_cyan    = 6
+local dark_white   = 7
 
-local bright_red     = 9  -- Bright Red
-local bright_green   = 10 -- Bright Green
-local bright_yellow  = 11 -- Bright Yellow
-local bright_blue    = 12 -- Bright Blue
-local bright_magenta = 13 -- Bright Magenta
-local bright_cyan    = 14 -- Bright Cyan
+local bright_black   = 8
+local bright_red     = 9
+local bright_green   = 10
+local bright_yellow  = 11
+local bright_blue    = 12
+local bright_magenta = 13
+local bright_cyan    = 14
+local bright_white   = 15
 
 ----------------------------
 -- User Interface Objects --
@@ -47,7 +51,7 @@ hl("VertSplit",  { reverse = false })
 hl("Visual",     { ctermfg = white, ctermbg = grey })
 
 hl("CursorLineNr", { ctermfg = bright_red, underline = false })
-hl("LineNr",       { ctermfg = grey })
+hl("LineNr",       { ctermfg = bright_black })
 
 ------------------
 -- Text Objects --
