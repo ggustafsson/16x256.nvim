f8c304107cd2248da7e6a533e10d811ede27e283
Göran Gustafsson <gustafsson.g@gmail.com>
2022-04-22 08:33:14 +0200

    ...

--- colors/16x256.lua
+++ colors/16x256.lua
@@ -12,19 +12,21 @@ vim.cmd [[
 
 vim.g.colors_name = "16x256"
 
+local fg_black = 16
+local fg_white = 231
+
 function hl(group, colors)
   vim.api.nvim_set_hl(0, group, colors)
 end
-
 
 ----------------------------
 -- User Interface Objects --
 ----------------------------
 
-hl("Search",     { ctermfg = 16, ctermbg = 11 })
+hl("Search",     { ctermfg = fg_black, ctermbg = 11 })
 hl("SignColumn", { ctermfg = 14, ctermbg = none })
 hl("VertSplit",  { reverse = false })
-hl("Visual",     { ctermfg = 231, ctermbg = 242 })
+hl("Visual",     { ctermfg = fg_white, ctermbg = 242 })
 
 ------------------
 -- Text Objects --
