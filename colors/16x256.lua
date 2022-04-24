62183ec7e727efa5ae4b854348c0a66fe2ae3647
Göran Gustafsson <gustafsson.g@gmail.com>
2022-04-24 22:21:03 +0200

    static dark bright

--- colors/16x256.lua
+++ colors/16x256.lua
@@ -18,50 +18,56 @@ end
 ---------------------
 -- Color Variables --
 ---------------------
-local black = 16  -- #000000
-local grey  = 240
-local white = 231 -- #ffffff
+local static = {
+  black = 16,  -- #000000
+  grey  = 240,
+  white = 231, -- #ffffff
+}
 
-local dark_black   = 0
-local dark_red     = 1
-local dark_green   = 2
-local dark_yellow  = 3
-local dark_blue    = 4
-local dark_magenta = 5
-local dark_cyan    = 6
-local dark_white   = 7
+local dark = {
+  black   = 0,
+  red     = 1,
+  green   = 2,
+  yellow  = 3,
+  blue    = 4,
+  magenta = 5,
+  cyan    = 6,
+  white   = 7,
+}
 
-local bright_black   = 8
-local bright_red     = 9
-local bright_green   = 10
-local bright_yellow  = 11
-local bright_blue    = 12
-local bright_magenta = 13
-local bright_cyan    = 14
-local bright_white   = 15
+local bright = {
+  black   = 8,
+  red     = 9,
+  green   = 10,
+  yellow  = 11,
+  blue    = 12,
+  magenta = 13,
+  cyan    = 14,
+  white   = 15,
+}
 
 ---------------------
 -- Highlight Rules --
 ---------------------
 hl("MatchParen", { ctermbg = none, underline = true })
-hl("Search",     { ctermfg = black, ctermbg = dark_yellow })
-hl("SignColumn", { ctermfg = bright_cyan, ctermbg = none })
-hl("Todo",       { ctermfg = bright_cyan, underline = true })
+hl("Search",     { ctermfg = static.black, ctermbg = dark.yellow })
+hl("SignColumn", { ctermfg = bright.cyan, ctermbg = none })
+hl("Todo",       { ctermfg = bright.cyan, underline = true })
 hl("VertSplit",  { reverse = false })
-hl("Visual",     { ctermfg = white, ctermbg = grey })
+hl("Visual",     { ctermfg = static.white, ctermbg = static.grey })
 
-hl("CursorLineNr", { ctermfg = bright_red, underline = false })
-hl("LineNr",       { ctermfg = bright_black })
+hl("CursorLineNr", { ctermfg = bright.red, underline = false })
+hl("LineNr",       { ctermfg = bright.black })
 
-hl("NonText",    { ctermfg = bright_black })
-hl("SpecialKey", { ctermfg = bright_black })
+hl("NonText",    { ctermfg = bright.black })
+hl("SpecialKey", { ctermfg = bright.black })
 
 --------------------
 -- Filetype: diff --
 --------------------
-hl("diffAdded",   { ctermfg = bright_green })
-hl("diffLine",    { ctermfg = bright_cyan })
-hl("diffRemoved", { ctermfg = bright_red })
+hl("diffAdded",   { ctermfg = bright.green })
+hl("diffLine",    { ctermfg = bright.cyan })
+hl("diffRemoved", { ctermfg = bright.red })
 
 -- XXX: I can't figure out how to do this without vim.cmd().
 vim.cmd [[
@@ -73,4 +79,4 @@ vim.cmd [[
 ---------------------------
 -- Plugin: nvim-tree.lua --
 ---------------------------
-hl("NvimTreeFolderIcon", { ctermfg = bright_blue })
+hl("NvimTreeFolderIcon", { ctermfg = bright.blue })
