36e94c06da3f4107e1031771d542ba3350c2828e
Göran Gustafsson <gustafsson.g@gmail.com>
2022-05-07 19:07:01 +0200

    NvimTreeGitDirty

--- colors/16x256.lua
+++ colors/16x256.lua
@@ -82,5 +82,5 @@ vim.cmd [[
 ---------------------------
 -- Plugin: nvim-tree.lua --
 ---------------------------
-hl("NvimTreeFileDirty",  { ctermfg = bright.yellow })
 hl("NvimTreeFolderIcon", { ctermfg = bright.blue })
+hl("NvimTreeGitDirty",   { ctermfg = bright.yellow })
